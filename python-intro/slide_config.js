ddc332f70a408dc3c31f665c2ea8b7db7ddd3b07
Alejandro Gómez <alejandro@dialelo.com>
2015-03-24 22:52:05 +0100

    WIP python intro workshop

---
+++ python-intro/slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Introducción a Python', 
+     subtitle: '', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/python-icon.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'Alejandro Gómez', 
+     company: 'Kaleidos', 
+     gplus: '', 
+     twitter: 'dialelo', 
+     www: 'http://dialelo.github.io', 
+     github: 'http://github.com/dialelo', 
+   }]
+};
